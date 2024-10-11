a1c293068d71f5959b71473c759649d2fa6b9587
Tran Thanh Duy <thanhduyct03@gmail.com>
2024-10-11 21:37:13 +0700

    xong header footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './header';
-import { Footer } from 'antd/es/layout/layout';
+import Footer from './footer';
 
 const Layout = () => {
     return (
