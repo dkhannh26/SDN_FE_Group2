9c25e53d9e6f8cad926f96ce00954c5925b162fe
Tran Thanh Duy <thanhduyct03@gmail.com>
2024-10-15 15:42:28 +0700

    add reset pass header

--- src/components/login.js
+++ src/components/login.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { PATH } from "../config/api.config";
 
-import { Button, Form, Input, notification } from "antd";
+import { Button, Carousel, Form, Input, notification } from "antd";
 import { useNavigate } from "react-router-dom";
 const LoginPopover = () => {
   const navigate = useNavigate();
@@ -70,68 +70,130 @@ const LoginPopover = () => {
     // console.log("Success:", res);
   };
 
+  // const onChange = (currentSlide) => {
+  //   console.log(currentSlide);
+  // };
+
+  const carouselRef = useRef(null);
+
+  // const gotoSlide = (slideIndex) => {
+  //   if (carouselRef.current) {
+  //     carouselRef.current.goTo(slideIndex, true);
+  //   }
+  // };
+
   return (
-    <form className="login-pop" onSubmit={onSubmit}>
-      <div className="login-pop-title">
-        <p className="login-pop-title__1">Đăng nhập tài khoản</p>
-        <p className="login-pop-title__2 text">
-          Nhập email và mật khẩu của bạn:
-        </p>
-      </div>
-      <div className="login-pop-input">
-        <input
-          name="email"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <label className="text">Username</label>
-      </div>
-      <div className="login-pop-input">
-        <input
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          type="password"
-        />
-        <label className="text">Password</label>
-      </div>
-      <div className="login-pop-recaptcha">
-        This site is protected by reCAPTCHA and the Google
-        <a
-          href="https://policies.google.com/privacy"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Privacy Policy{" "}
-        </a>
-        and{" "}
-        <a
-          href="https://policies.google.com/terms"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Terms of Service{" "}
-        </a>
-        apply.
-      </div>
-      <button type="submit" className="login-pop-btn">
-        <span>ĐĂNG NHẬP</span>
-      </button>
-      <div className="login-pop-navigate text">
-        <p>
-          Khách hàng mới?
-          <a href="/"> Tạo tài khoản</a>
-        </p>
-        <p>
-          Quên mật khẩu?
-          <a href="/"> Khôi phục mật khẩu</a>
-        </p>
-      </div>
-    </form>
+    <Carousel ref={carouselRef} dots={false} style={{ width: 350, }}>
+      <form className="login-pop" onSubmit={onSubmit} >
+        <div className="login-pop-title">
+          <p className="login-pop-title__1">Đăng nhập tài khoản</p>
+          <p className="login-pop-title__2 text">
+            Nhập email và mật khẩu của bạn:
+          </p>
+        </div>
+        <div className="login-pop-input">
+          <input
+            name="email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <label className="text">Username</label>
+        </div>
+        <div className="login-pop-input">
+          <input
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type="password"
+          />
+          <label className="text">Password</label>
+        </div>
+        <div className="login-pop-recaptcha">
+          This site is protected by reCAPTCHA and the Google
+          <a
+            href="https://policies.google.com/privacy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Privacy Policy{" "}
+          </a>
+          and{" "}
+          <a
+            href="https://policies.google.com/terms"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Terms of Service{" "}
+          </a>
+          apply.
+        </div>
+        <button type="submit" className="login-pop-btn">
+          ĐĂNG NHẬP
+        </button>
+        <div className="login-pop-navigate text">
+          <p>
+            Khách hàng mới?
+            <a href="/"> Tạo tài khoản</a>
+          </p>
+          <p>
+            Quên mật khẩu?
+            <a href="#" onClick={(e) => { e.preventDefault(); carouselRef.current.next(); }} > Khôi phục mật khẩu</a>
+          </p>
+        </div>
+      </form>
+
+      <form>
+        <div className="login-pop-title">
+          <p className="login-pop-title__1">KHÔI PHỤC MẬT KHẨU</p>
+          <p className="login-pop-title__2 text">
+            Nhập email của bạn:
+          </p>
+        </div>
+        <div className="login-pop-input">
+          <input
+            name="email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <label className="text">Username</label>
+        </div>
+        <div className="login-pop-recaptcha">
+          This site is protected by reCAPTCHA and the Google
+          <a
+            href="https://policies.google.com/privacy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Privacy Policy{" "}
+          </a>
+          and{" "}
+          <a
+            href="https://policies.google.com/terms"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Terms of Service{" "}
+          </a>
+          apply.
+        </div>
+        <button type="submit" className="re-pass-btn">
+          KHÔI PHỤC
+        </button>
+        <div className="login-pop-navigate text">
+          <p>
+            Bạn đã nhớ mật khẩu?
+            <a href="#" onClick={(e) => { e.preventDefault(); carouselRef.current.prev(); }} > Trở về đăng nhập</a>
+          </p>
+        </div>
+      </form>
+    </Carousel>
   );
 };
 
