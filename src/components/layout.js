c500e8e718406548cdd9d6df8940cefb9f415e76
Tran Thanh Duy <thanhduyct03@gmail.com>
2024-10-22 16:40:02 +0700

    add home page

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './header';
 import Footer from './footer';
+import Home from '../pages/Home';
 
 const Layout = () => {
     return (
         <>
             <Header/>
-            <Outlet></Outlet>
+            <Outlet>
+                
+            </Outlet>
             <Footer/>
         </>
     );
--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { Outlet, Route } from "react-router-dom";
 import Layout from "../components/layout";
+import Product from '../pages/Product';
+import Home from "../pages/Home";
 
 export const CustomerRoutes = (
-  <Route path="customer" element={<Layout />}></Route>
+  <Route path="customer" element={<Layout />}>
+    <Route path="product" element={<Product />}></Route>
+    <Route path="" element={<Home />}></Route>
+  </Route>
 );
